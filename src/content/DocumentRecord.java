71226e2a8715dd0a5601ec281ac49cc4d204c414
John Colagioia <john@colagioia.net>
2015-05-19 06:44:44 -0400

    Add record to reflect searches

---
+++ src/content/DocumentRecord.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package content;
+
+/**
+ * @author john
+ *
+ */
+public class DocumentRecord {
+	public int id, size;
+	public String file_type, digest;
+	public boolean folder;
+	public String created, created_at, updated_at;
+
+	/**
+	 * 
+	 */
+	public DocumentRecord() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
