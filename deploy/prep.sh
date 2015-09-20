726b2bc743dbe65fccf0be57563c1d9667ea2023
John Colagioia <john@colagioia.net>
2015-09-20 07:05:31 -0400

    Clean up preparation script

--- deploy/prep.sh
+++ deploy/prep.sh
@@ -4,18 +4,18 @@ infile=template.html
 outdir=stage
 outfile=$base.html
 applet=$base.jar
-keys=comicscanner.keys
+#keys=comicscanner.keys
 jarlist=$applet
 mkdir -p $outdir
 rm -rf $outdir/*
 #keytool -genkey -keystore $keys -alias me -storepass $1
 #keytool -selfcert -keystore $keys -alias me -storepass $1
 #jarsigner -keystore $keys $applet me -storepass $1
-mv *.jar $outdir
+mv ./*.jar $outdir
 for jar in $(grep "<classpathentry kind=\"lib\" path=\".*\.jar\"/>" ../.classpath | cut -f4 -d'"')
 do
-  cp -u $jar $outdir
-  base=$(basename $jar)
+  cp -u "$jar" $outdir
+  base=$(basename "$jar")
   jarlist=$jarlist,$base
 done
 sed "s/===/$jarlist/g" $infile > $outdir/$outfile
